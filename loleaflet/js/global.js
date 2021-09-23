01b50b0d46cdf535602c1762a21e5a7f52097e2f
Efim.Dezhin <efim.dezhin@g42.ai>
2021-09-23 10:59:16 +0400

    rollback websocket url from not secure to secure

--- loleaflet/js/global.js
+++ loleaflet/js/global.js
@@ -731,7 +731,7 @@
 	} else {
 		var websocketURI = global.host + global.serviceRoot + '/lool/' + encodeURIComponent(global.docURL + (docParams ? '?' + docParams : '')) + '/ws' + wopiSrc;
 		try {
-			global.socket = global.createWebSocket(websocketURI.replace('wss://', 'ws://'));
+			global.socket = global.createWebSocket(websocketURI);
 		} catch (err) {
 			console.log(err);
 		}
